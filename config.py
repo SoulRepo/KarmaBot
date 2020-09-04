2f2ac0ea7da89d92f5b5d594871a43913539e6ae
SoulRepo <24778461+SoulRepo@users.noreply.github.com>
2020-09-03 21:31:16 -0400

    added comments

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
+# personal use script code
 C_ID = ''
+# secret on reddit apps
 C_S  = ''
+# reddit app's description
 U_A = ''
+# username of bot
 UN = ''
+# password of bot (use throwaway if not safe, you should be anyways)
 PW = ''
+# imgur client id
 I_ID = ''
